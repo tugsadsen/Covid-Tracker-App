f1e46796478d32d092ef66b5aab0feb705d2b130
tugsadsen <tugsad07@gmail.com>
2022-04-25 17:16:59 +0300

    Initial Commit

---
+++ COVID Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COVID Tracker
+//
+//  Created by Tuğşad Şen on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
